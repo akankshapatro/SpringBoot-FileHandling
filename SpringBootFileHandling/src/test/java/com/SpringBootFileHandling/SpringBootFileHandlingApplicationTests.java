44785787d2974c847cc7fc87e9dfcf6717b27b50
akank <akank@192.168.1.39>
2022-10-26 14:30:46 +0530

    Spring Boot File Handling- API's to handle files

---
+++ SpringBootFileHandling/src/test/java/com/SpringBootFileHandling/SpringBootFileHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootFileHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
